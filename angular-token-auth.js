a0f99fa4a9c38b4e3b9a5ca24206e41fd899d619
Perry Roper <perryroper@gmail.com>
2013-11-14 14:56:20 +0000

    Add initial js

---
+++ angular-token-auth.js
@@ -0,0 +1,94 @@
+'use strict';
+
+var auth = angular.module('ngTokenAuth', ['ng', 'ngCookies', 'settings']);
+
+// Default settings. You can override these in your settings module.
+auth.constant('TOKEN_AUTH', {
+    ENDPOINT: '/auth/',
+    LOGIN: '/login/',
+    LOGOUT: '/logout/'
+});
+
+auth.config(['$routeProvider', 'TOKEN_AUTH', 'SETTINGS', function ($routeProvider, TOKEN_AUTH, SETTINGS) {
+    var MODULE_SETTINGS = angular.extend({}, TOKEN_AUTH, SETTINGS.TOKEN_AUTH);
+
+    $routeProvider
+        .when(MODULE_SETTINGS.LOGIN, {
+            templateUrl: 'views/auth/login.html',
+            controller: 'LoginCtrl',
+            anonymous: true
+        })
+        .when(MODULE_SETTINGS.LOGOUT, {
+            templateUrl: 'views/auth/logout.html',
+            controller: 'LogoutCtrl',
+            anonymous: true
+        });
+}]);
+
+auth.run(['$rootScope', '$location', '$user', 'TOKEN_AUTH', 'SETTINGS', function ($rootScope, $location, $user, TOKEN_AUTH, SETTINGS) {
+    var MODULE_SETTINGS = angular.extend({}, TOKEN_AUTH, SETTINGS.TOKEN_AUTH);
+
+    $rootScope.$on('$routeChangeStart', function (e, next, current) {
+        if (next.$$route && !next.$$route.anonymous && !$user.authenticated) {
+            $location.url(MODULE_SETTINGS.LOGIN + '?next='+ next.$$route.originalPath);
+        }
+    });
+}]);
+
+auth.controller('LoginCtrl', ['$scope', '$location', '$user', function ($scope, $location, $user) {
+    if($user.authenticated) {
+        $location.url('/');
+    }
+
+    $scope.login = function () {
+        $user.login($scope.email, $scope.password).then(function (data) {
+            $location.url($location.search().next || '/');
+        });
+    };
+}]);
+
+auth.controller('LogoutCtrl', ['$scope', '$user', function ($scope, $user) {
+    $user.logout();
+}]);
+
+auth.factory('$user', ['$rootScope', '$q', '$http', '$cookieStore', '$location', 'TOKEN_AUTH', 'SETTINGS', function ($rootScope, $q, $http, $cookieStore, $location, TOKEN_AUTH, SETTINGS) {
+    var MODULE_SETTINGS = angular.extend({}, TOKEN_AUTH, SETTINGS.TOKEN_AUTH);
+
+    var user = {
+        authenticated: false,
+        token: null,
+        // scope: $rootScope.$new(),
+        login: function (email, password) {
+            var user = this;
+            var deferred = $q.defer();
+            $http.post(SETTINGS.API_ROOT + MODULE_SETTINGS.ENDPOINT, {
+                username: email,
+                password: password
+            }).success(function(data) {
+                user.authenticated = true;
+                user.token = data.token;
+                $cookieStore.put('auth', {
+                    token: data.token
+                });
+                deferred.resolve(user);
+            }).error(function (data) {
+                deferred.reject(data);
+            });
+            return deferred.promise;
+        },
+        logout: function () {
+            $cookieStore.remove('auth');
+            user.token = null;
+            user.authenticated = false;
+            $location.url(MODULE_SETTINGS.LOGIN);
+        }
+    };
+
+    var cookie = $cookieStore.get('auth');
+    if (cookie && cookie.token) {
+        user.authenticated = true;
+        user.token = cookie.token;
+    }
+
+    return user;
+}]);
