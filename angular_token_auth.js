60623911e4a769a378fb315bc0ccef67b4e648a6
Perry Roper <perryroper@gmail.com>
2014-10-24 23:05:41 +0100

    Add flag to route to force anonymous only routes.

--- angular_token_auth.js
+++ angular_token_auth.js
@@ -19,7 +19,8 @@
         $routeProvider
             .when(MODULE_SETTINGS.LOGIN, {
                 templateUrl: 'templates/auth/login.html',
-                controller: 'LoginCtrl'
+                controller: 'LoginCtrl',
+                anonymousOnly: true
             })
             .when(MODULE_SETTINGS.LOGOUT, {
                 templateUrl: 'templates/auth/logout.html',
@@ -65,18 +66,32 @@
 
         $rootScope.$on('$routeChangeStart', function (e, next, current) {
             var nextRoute = next.$$route;
+
             // By default, all routes should be anonymous.
             var nextRouteIsAnonymous = true;
             if (angular.isDefined(nextRoute) && nextRoute.anonymous === false) {
                 nextRouteIsAnonymous = false;
             }
 
-            // If the next route isn't anonymous and a token doesn't exist,
-            // redirect to the log in page with a `next` parameter set to the
-            // anonymous path.
-            if (!nextRouteIsAnonymous && !authFactory.getToken()) {
-                $location.url(MODULE_SETTINGS.LOGIN + '?next=' + $location.path());
-                $location.replace();
+            var nextRouteAnonymousOnly = false;
+            if (angular.isDefined(nextRoute) && nextRoute.anonymousOnly === true) {
+                nextRouteAnonymousOnly = true;
+            }
+
+            if (authFactory.getToken()) {
+                // If the next route is public only and we are logged in then redirect to the
+                // login redirect URL.
+                if (nextRouteAnonymousOnly) {
+                    $location.url(MODULE_SETTINGS.LOGIN_REDIRECT_URL);
+                }
+            } else {
+                // If the next route isn't anonymous and a token doesn't exist,
+                // redirect to the log in page with a `next` parameter set to the
+                // anonymous path.
+                if (!nextRouteIsAnonymous) {
+                    $location.url(MODULE_SETTINGS.LOGIN + '?next=' + $location.path());
+                    $location.replace();
+                }
             }
         });
 
