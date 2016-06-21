1c06dddf00647e7de1a8a402bf85205cec02a18a
Ming Lee <ming@incuna.com>
2016-06-21 16:15:25 +0100

    Add an init file.

---
+++ src/init.js
@@ -0,0 +1,20 @@
+(function (angular) {
+    'use strict';
+
+    angular.module('angular-token-auth', [
+        'angular-token-auth.auth-login',
+        'angular-token-auth.auth-logout',
+        'angular-token-auth.auth-login-form',
+        'angular-token-auth.auth-actions',
+        'angular-token-auth.auth',
+        'angular-token-auth.auth-interceptor',
+        'angular-token-auth.auth-login-form-directive-factory',
+        'angular-token-auth.auth-login-form',
+        'angular-token-auth.auth-module-settings',
+        'angular-token-auth.auth-route-change-start',
+        'angular-token-auth.auth-storage',
+        'angular-token-auth.route-config',
+        'angular-token-auth.constants'
+    ]);
+
+}(window.angular));
