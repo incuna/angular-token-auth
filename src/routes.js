8d016611fce8b5c09fb59a9ef1935da248d23e38
Ming Lee <ming@incuna.com>
2016-06-22 08:43:50 +0100

    Commit the routes file.

---
+++ src/routes.js
@@ -0,0 +1,24 @@
+(function (angular) {
+    'use strict';
+
+    var module = angular.module('angular-token-auth.routes', [
+        'angular-token-auth.constants',
+        'project_settings'
+    ]);
+
+    module.config(['$routeProvider','TOKEN_AUTH', 'PROJECT_SETTINGS', function ($routeProvider, TOKEN_AUTH, PROJECT_SETTINGS) {
+        var MODULE_SETTINGS = angular.extend({}, TOKEN_AUTH, PROJECT_SETTINGS.TOKEN_AUTH);
+
+        $routeProvider
+            .when(MODULE_SETTINGS.LOGIN, {
+                templateUrl: 'templates/auth/login.html',
+                controller: 'LoginCtrl',
+                anonymousOnly: true
+            })
+            .when(MODULE_SETTINGS.LOGOUT, {
+                templateUrl: 'templates/auth/logout.html',
+                controller: 'LogoutCtrl'
+            });
+    }]);
+
+}(window.angular));
