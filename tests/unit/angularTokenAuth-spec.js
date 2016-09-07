708048362728b7cd52b805a05b2ad33617c07b04
Ming Lee <ming@incuna.com>
2016-09-07 09:01:06 +0100

    Undo changes to tokenAuth-spec

--- tests/unit/angularTokenAuth-spec.js
+++ tests/unit/angularTokenAuth-spec.js
@@ -1,4 +1,4 @@
-/* global beforeEach, module, describe, it, expect */
+/* global beforeEach, module, inject, describe, it, expect */
 
 (function () {
 
@@ -8,6 +8,18 @@
 
         // Load the generic module.
         beforeEach(module('angular-token-auth'));
+
+        beforeEach(function () {
+
+            // Inject dependencies. module() can no longer be called after this.
+            inject(function ($rootScope) {
+                // Inject something to create the injector.
+                // Uncomment this line if you are having test troubles. The
+                // injector won't run if there are missing dependencies.
+                // console.log('generic: If this log does not show, there are missing dependencies.');
+            });
+
+        });
 
         // Replace this test when others are added. A running test is all that's
         // needed to see if the dependencies load without error.
